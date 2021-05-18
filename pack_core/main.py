a8e1fb0217b9876f0db96413d188ad33d022eaa2
i.kozlov <kozlov.titan2010@yandex.ru>
2021-05-18 09:45:22 +0300

    WorkFlow. Ivan Kozlov. Обобщение поиска словарей

--- pack_core/main.py
+++ pack_core/main.py
@@ -7,6 +7,11 @@ from tortoise.backends.base.config_generator import expand_db_url
 from MODS.scripts.python.easy_scripts import PROJECT_GENERAL_FOLDER
 from MODS.standart_namespace.models import asf
 
+from MODS.rest_core.pack_core.RUN.__tools.tortoise import models_inspector
+
+from importlib import import_module
+from inspect import getmembers, isclass
+
 """
 Defaults
 """
@@ -202,3 +207,28 @@ def ycl_get_connection_settings(url):
         'database': credentials['database'],
     }
     return settings
+
+
+"""
+META
+"""
+
+
+def get_orm_class(client_key, dict_name):
+    """
+    Получить класс модели ОРМ по ключу клиента и имени словаря
+    Имена совпадают один к одному в конфигурации метаданных
+    """
+    for mod_name in models_inspector():
+        mod = import_module(mod_name)
+        class_models = (
+            m[1] for m in getmembers(mod, isclass)
+            if m[1].__module__ == mod.__name__
+               and client_key == getattr(m[1], '__original_client_name__', '')
+               and dict_name == getattr(m[1], '__original_dict_name__', '')
+        )
+
+        for class_obj in class_models:
+            return class_obj
+
+    return None
